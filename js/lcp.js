954f605d4b1cc5e67880f3dc71218f1028bb6eed
arjen <info@corewebvitals.io>
2024-10-18 10:50:22 +0200

    prod version 1

--- js/lcp.js
+++ js/lcp.js
@@ -4,10 +4,14 @@ new PerformanceObserver((entryList) => {
     console.log("LCP candidate:", entry.startTime, entry);
     let timeinms = entry.startTime.toFixed(0);
 
+    if (timeinms < 1) {
+      return;
+    }
+
     // save to session storage for the url, keep the last 50 values per url
     let url = window.location.href;
 
-    let filename = url.substring(url.lastIndexOf('/') + 1);
+    let filename = url.substring(url.lastIndexOf("/") + 1);
 
     let lcpValues = sessionStorage.getItem(filename);
     if (lcpValues) {
@@ -16,14 +20,14 @@ new PerformanceObserver((entryList) => {
       lcpValues = [];
     }
     lcpValues.push(timeinms);
-    if (lcpValues.length > 5) {
+    if (lcpValues.length > 50) {
       lcpValues.shift();
     }
     sessionStorage.setItem(filename, JSON.stringify(lcpValues));
 
-    console.log(lcpValues);
+    let avg = lcpValues.reduce((a, b) => parseInt(a) + parseInt(b), 0) / lcpValues.length;
 
     // update the text content of the element
-    lcptimingNode.textContent = `${timeinms} ms (avg for this version = ${lcpValues.reduce((a, b) => parseInt(a) + parseInt(b), 0) / lcpValues.length} ms)`;
+    lcptimingNode.textContent = `${timeinms} ms (avg for this version = ${avg.toFixed(0)}ms, count = ${lcpValues.length})`;
   }
 }).observe({ type: "largest-contentful-paint", buffered: true });
